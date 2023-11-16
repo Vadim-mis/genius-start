19b169a93e3d3ac8788913aeccbd712629d4e9e3
Vadim <mishanchukv@gmail.com>
2023-11-16 16:01:14 +0200

    My first commit

---
+++ Untitled-1.js
@@ -0,0 +1,6 @@
+
+let message = "Yuo like"
+let number = 10
+
+if (number > 5) {console.log(message)}
+else {console.log("You bad")}
---
+++ test.js
@@ -0,0 +1,73 @@
+// const x=1// number
+// let y=2
+// const word="helo world"// string
+// const z="2"
+// const isGood=true //bool
+// console.log (x+y, word, z-y)
+
+// if
+// for
+// while
+
+
+
+// let message = "Yuo like"
+// let number = 10
+// console.log (number > 11)
+
+
+// if (number >= 11) {
+//     console.log(message)
+// } else {
+//     console.log("You bad")
+// }
+
+// let x=12
+// let y=10
+// let operation = "+"
+// let sum = null
+// if (operation === "+") {
+//     sum = x + y
+// } else if (operation === "-") {
+//     sum = x - y
+// } else if (operation === "/") {
+//     sum = x / y
+// } else if (operation === "*") {
+//     sum = x * y
+// } 
+// console.log (sum)
+
+// let x=10
+// if (x%2 === 1) {
+//     console.log("Yes")
+// } else {
+//     console.log("No")
+// }
+
+// let x=7
+// if ((x%2 === 0) || (x === 5)) {
+//     console.log("Yes")
+    
+// } else {
+//     console.log("No")
+//     if (x === 5) {
+//         console.log("Yes")
+//     } else {
+//         console.log("No")
+//     }
+// }
+
+// let num = 0
+// while (num < 5) {
+//     console.log(num)
+//     num++
+// }
+// let sum = 0
+// for (let num = 0; num < 1000; num++) {
+//     if ((num%3 === 0) || (num%5 === 0)){
+//         sum = sum + num 
+//         console.log(num)
+//     }
+// }
+// console.log (sum)
+
